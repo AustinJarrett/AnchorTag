fc09aba8a273cbc9b2100956854e37ee8003d9d6
Austin Jarrett <ajarrett@harvestgroup.com>
2017-07-17 09:34:05 -0500

    remove console line in main

--- AnchorTag/Program.cs
+++ AnchorTag/Program.cs
@@ -12,8 +12,6 @@ namespace AnchorTag
     {
         static void Main(string[] args)
         {
-
-            Console.WriteLine("Running");
             string str = File.ReadAllText("input.txt");
             File.WriteAllText("output.txt", "");
 
